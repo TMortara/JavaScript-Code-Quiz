9d154889882aa3c9d838680ec5de1995754fdeb0
Teresa <107971753+TMortara@users.noreply.github.com>
2022-11-12 15:55:42 -0600

    add buttons to HTML and add questions and answer to JS file

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+var quizQuestions = [
+    {
+        question: "What is a function contatined in a object called?",
+        answer: [
+            {text: "function", correct: false},
+            {text: "array", correct: false},
+            {text: "property", correct: false},
+            {text: "method", correct: true}
+        ]
+    },
+    {
+        question: "Which operator is used to determine strict equal?",
+        answer: [
+            {text: "==", correct: false},
+            {text: "!==", correct: false},
+            {text: "===", correct: true},
+            {text: "=", correct: false}
+        ]
+    },
+    {
+        question: "What data type returns a true or false value?",
+        answer: [
+            {text: "boolean", correct: true},
+            {text: "string", correct: false},
+            {text: "number", correct: false},
+            {text: "undefined", correct: false}
+        ]
+    },
+    {
+        question: "Which character terminates a statement?",
+        answer: [
+            {text: "!", correct: false},
+            {text: ";", correct: true},
+            {text: ".", correct: false},
+            {text: "^", correct: false}
+        ]
+    }, 
+    {
+        question: "What method do you use to generate a random number between 0 and 1?",
+        answer: [
+            {text: "Math.round()", correct: false},
+            {text: "Math.ceil()", correct: false},
+            {text: "Math.floor()", correct: false},
+            {text: "Math.random()", correct: true}
+        ]
+    }, 
+    
+]
