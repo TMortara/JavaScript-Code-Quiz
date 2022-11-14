8757edeefa25c1701889785a395c17aafbd0a803
Teresa <107971753+TMortara@users.noreply.github.com>
2022-11-14 17:18:42 -0600

    add css reset file, complete high scores page, initial CSS updates

--- assets/js/script-scores.js
+++ assets/js/script-scores.js
@@ -8,14 +8,23 @@ var scoresInputEl = document.querySelector("#scoresInput");
 
 // High scores
 function renderHighscores() {
-    scoresInputEl.innerHTML = " ";
-    highScores = JSON.parse(localStorage.getItem("highscores"));
+    scoresInputEl.innerHTML = "";
+    var highScores = JSON.parse(localStorage.getItem("highscores")) || [];
     for (var i = 0; i < highScores.length; i++) {
-        var scoreLine = highScores[i];
-        var li = document.createElement("li");
-        li.textContent = highScores;
-        li.setAttribute("data-index", i);
-        highScoresEl.appendChild(li);
+        var scoreLine = document.createElement("li");
+        scoreLine.textContent = `${highScores[i].userInitials} - ${highScores[i].finalScore}`;
+        scoreLine.setAttribute("data-index", i);
+        highScoresEl.appendChild(scoreLine);
 }
 }
+renderHighscores();
+
+function removeHistory(event){
+    event.preventDefault()
+    localStorage.setItem('highscores', JSON.stringify([]))
+    window.location.reload()
+}
+
+document.getElementById('clearScores-btn').addEventListener('click', removeHistory)
+
 // TODO: Set new submission to local storage
