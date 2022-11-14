ce677b065dea8091a1f0e6a2322ffa9c86a906a6
Teresa <107971753+TMortara@users.noreply.github.com>
2022-11-13 20:32:59 -0600

    add links to scores html and remove next button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,21 @@
 var headerEl = document.querySelector("#header");
-// Buttons
-var timerEl = document.querySelector("#timer");
+var displayMessageEl = document.querySelector("#displayMessage");
 var currentScoreEl = document.querySelector("#currentScore");
 var introEl = document.querySelector("#intro");
+var inputHSEl = document.querySelector("#inputHS");
+// Buttons
+var timerEl = document.querySelector("#timer");
 var startBtnEl = document.querySelector("#start-btn");
 var nextBtnEl = document.querySelector("#next-btn");
+var submitInitialsBtnEl = document.querySelector("#submitInitials-btn");
 // Quiz Controls
 var quizEl = document.querySelector("#quizContainer");
 var questionsEl = document.querySelector("#question");
 var choicesEl = document.querySelector("#choices");
-// Universal Variables 
-// var timer = 0;
+// Global Variables 
 var totalTime = 60;
 var score = 0;
+var interval;
 // Quiz Questions
 var quizQuestions = [
     {
@@ -44,26 +47,20 @@ var quizQuestions = [
 ]
 var questionAskedIndex = 0;
 
-function startQuestions() {
-    var currentQuestion = quizQuestions[questionAskedIndex].title;
-    questionsEl.textContent = currentQuestion;
-    var CurrentQuestionAnswers = quizQuestions[questionAskedIndex].choices;
-    for (i = 0; i < choicesEl.children.length; i++) {
-        choicesEl.children[i].textContent = `${quizQuestions[questionAskedIndex].choices[i]}`;
-    }
-}
+startBtnEl.addEventListener("click", function () {
+    startTimer();
+    startQuiz();
+    show(timerEl);
+    // setNextQuestion();
+    // renderQuestion();
+    // show(quizEl);
+});
 
-function nextQuestion() {
-    
-}
-
-
-// Timer functions
 function startTimer() {
-    timerEl.textContent = totalTime;
-    setInterval(function() {
+    timerEl.textContent = "Timer " + totalTime;
+    interval = setInterval(function() {
         if (totalTime >= 0) {
-        timerEl.textContent = totalTime;
+        timerEl.textContent = "Timer " + totalTime;
         totalTime--;
         } 
     }, 500); 
@@ -74,14 +71,6 @@ function stopTimer() {
     clearInterval(interval);
 }
 
-startBtnEl.addEventListener("click", function () {
-    startTimer();
-    startQuiz();
-    // setNextQuestion();
-    // renderQuestion();
-    // show(quizEl);
-});
-
 function startQuiz() {
     hide(startBtnEl);
     hide(introEl);
@@ -89,6 +78,52 @@ function startQuiz() {
     show(header);
     startQuestions();
 }
+function startQuestions() {
+    var currentQuestion = quizQuestions[questionAskedIndex].title;
+    questionsEl.textContent = currentQuestion;
+    var CurrentQuestionAnswers = quizQuestions[questionAskedIndex].choices;
+    for (i = 0; i < choicesEl.children.length; i++) {
+        choicesEl.children[i].textContent = `${quizQuestions[questionAskedIndex].choices[i]}`;
+    }
+}
+function checkAnswer(answer) {
+    if (quizQuestions[questionAskedIndex].answer == answer.id) {
+        score += 1;
+        currentScoreEl.textContent = "Current Score " + score;
+        displayMessageEl.textContent = "Correct!";
+    }
+    else {
+        totalTime -= 10;
+        displayMessageEl.textContent = "Incorrect!";
+    }
+}
+
+choicesEl.addEventListener("click", function(e) {
+    if (e.target.matches("button")) {
+        checkAnswer(e.target);
+        nextQuestion();
+    }
+});
+
+function nextQuestion() {
+    questionAskedIndex++;
+    if (questionAskedIndex < quizQuestions.length) {
+        startQuestions();
+    } else {
+        stopTimer();
+        hide(quizEl);
+        show(inputHSEl);
+        hide(timerEl);
+        currentScoreEl.innerHTML = "Final Score " + score;
+    }
+}
+
+
+
+
+
+
+
 
 
 function hide(element) {
